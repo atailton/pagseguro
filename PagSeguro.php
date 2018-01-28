f263f9b25280a10c8b2b9a56a784edef52a3b99d
Atailton Tavares <atailtoncarlos@gmail.com>
2018-01-28 21:14:16 -0200

    Update PagSeguro.php

--- PagSeguro.php
+++ PagSeguro.php
@@ -1,3 +1,40 @@
+public function iniciaPagamentoAction() { //gera o código de sessão obrigatório para gerar identificador (hash)
+
+		//$id = (string) $this->params ()->fromRoute( 'confirma', null );
+
+		$data['token'] =''; //token teste SANDBOX
+
+				//$_SERVER['REMOTE_ADDR']
+		$emailPagseguro = "";
+
+		$data = http_build_query($data);
+		$url = 'https://ws.sandbox.pagseguro.uol.com.br/v2/sessions';
+
+		$curl = curl_init();
+
+		$headers = array('Content-Type: application/x-www-form-urlencoded; charset=ISO-8859-1'
+			);
+
+		curl_setopt($curl, CURLOPT_URL, $url . "?email=" . $emailPagseguro);
+		curl_setopt($curl, CURLOPT_POST, true);
+		curl_setopt( $curl,CURLOPT_HTTPHEADER, $headers );
+		curl_setopt( $curl,CURLOPT_RETURNTRANSFER, true );
+		curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, false);
+		curl_setopt($curl, CURLOPT_POSTFIELDS, $data);
+		//curl_setopt($curl, CURLOPT_HTTP_VERSION, CURL_HTTP_VERSION_1_1);
+		curl_setopt($curl, CURLOPT_HEADER, false);
+		$xml = curl_exec($curl);
+
+		curl_close($curl);
+
+		$xml= simplexml_load_string($xml);
+		$idSessao = $xml -> id;
+		echo $idSessao;
+		exit;
+		//return $codigoRedirecionamento;
+
+	}
+
 public function efetuaPagamentoCartao($dados) {
 
 
